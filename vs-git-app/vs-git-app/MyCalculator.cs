1e3f1341031437b1a17716b5ee4836afd4cc1d52
bitbuzz <alexmuleh@gmail.com>
2016-10-06 11:33:20 -0600

    Added my calculator

---
+++ vs-git-app/vs-git-app/MyCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vs_git_app
+{
+	public class MyCalculator
+	{
+		internal int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		internal int Subract(int a, int b)
+		{
+			return a - b;
+		}
+
+		internal int Divide(int a, int b)
+		{
+			return a / b;
+		}
+
+		internal int Multiply(int a, int b)
+		{
+			return a * b;
+		}
+	}
+}
